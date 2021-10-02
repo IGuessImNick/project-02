3c67dbf1ea94ca471bc9419aabe01b5e258cb26f
Nicholas Skinner <njskinner@ucdavis.edu>
2021-10-02 11:45:31 -0700

    added seeds json template

---
+++ models/Choices.js
@@ -0,0 +1,24 @@
+const { Model, DataTypes } = require('sequelize');
+// const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
+
+class Choices extends Model {
+
+}
+
+Choices.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+        },
+
+        text: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        }
+    }
+);
+
+module.exports = Choices;
