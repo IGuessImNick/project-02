a12cafda78db6663f73acda1e5e775719e67804f
Nicholas Skinner <njskinner@ucdavis.edu>
2021-10-05 18:25:02 -0700

    added some partials and started on authentication

--- models/Prompts.js
+++ models/Prompts.js
@@ -2,9 +2,7 @@ const { Model, DataTypes } = require('sequelize');
 // const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
 
-class Prompts extends Model {
-
-}
+class Prompts extends Model {}
 
 Prompts.init(
     {
