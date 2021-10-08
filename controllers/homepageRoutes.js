542bdf7e652b96e81e1c1d72d42c0e786176520e
Nicholas Skinner <njskinner@ucdavis.edu>
2021-10-07 18:33:38 -0700

    formatting

--- controllers/homepageRoutes.js
+++ controllers/homepageRoutes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const { User } = require('../models');
 const withAuth = require('../utils/auth');
 
-router.get('/homepage', async (req, res) => {
+router.get('/homepage', withAuth, async (req, res) => {
     try {
         await res.render('homepage');
     } catch (err) {
